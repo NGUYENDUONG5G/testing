b8286d00241fdd0b3143ad61747107193d644c16
NGUYENDUONG5G <apeach2901@gmail.com>
2025-09-26 17:32:04 +0700

    repair part 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 
 public class Main {
-    public static double calculatePhone(double hour, double distance,boolean intraNet) {
+    public static double calculatePhone(double hour, double distance, boolean intraNet) {
         double res = 0.0;
-        double discountHour=0.0;
-        double discountDistance=0.0;
-        double price=890;
-        if(hour>=18||hour<8) discountHour=0.5;
-        if(distance>60 ) discountDistance=0.15;
-        if(!intraNet) price=990;
+        double discountHour = 0.0;
+        double discountDistance = 0.0;
+        double price = 890;
 
-        res=distance*price*(1-discountHour)*(1-discountDistance);
+        if (distance < 0) {
+            return -1;
+        }
+        if(hour<0||hour>24) return -1;
+        if (hour >= 18 || hour < 8) discountHour = 0.5;
+        if (distance > 60) discountDistance = 0.15;
+        if (!intraNet) price = 990;
+
+        res = distance * price * (1 - discountHour) * (1 - discountDistance);
 
         return res;
 
@@ -17,32 +22,34 @@ public class Main {
 
     public static void main(String[] args) {
         Object[][] testCases = {
-                {10.0, 50.0, true, 44500.0},
-                {20.0, 50.0, true, 22250.0},
+                {-1.0,0.0,true,-1.0},
+                {0.0, -1.0, true, -1.0},
+                {0.0, 30.0, true, 13350.0},
+                {10.0, 30.0, true, 26700.0},
+                {19.0, 30.0, true, 13350.0},
+                {0.0, 100.0, true, 37825.0},
                 {10.0, 100.0, true, 75650.0},
-                {20.0, 100.0, true, 37825.0},
-                {10.0, 50.0, false, 49500.0},
-                {20.0, 50.0, false, 24750.0},
+                {19.0, 100.0, true, 37825.0},
+                {0.0, 30.0, false, 14850.0},
+                {10.0, 30.0, false, 29700.0},
+                {19.0, 30.0, false, 14850.0},
+                {0.0, 100.0, false, 42075.0},
                 {10.0, 100.0, false, 84150.0},
-                {20.0, 100.0, false, 42075.0},
-                {8.0, 60.0, true, 53400.0},
-                {7.99, 60.0, true, 26700.0},
-                {18.0, 61.0, false, 25696.125}
+                {19.0, 100.0, false, 42075.0}
         };
         for (int i = 0; i < testCases.length; i++) {
+            String result = "PASS";
             double hour = (double) testCases[i][0];
             double distance = (double) testCases[i][1];
             boolean intraNet = (boolean) testCases[i][2];
             double expected = (double) testCases[i][3];
 
             double actualOput = calculatePhone(hour, distance, intraNet);
-            String result="PASS";
-            if(actualOput!=expected){
-                result="FAILED";
+            if (actualOput != expected) {
+                result = "FAILED";
             }
-
             System.out.printf("Case %d: hour=%.2f, distance=%.2f, intraNet=%s -> actualOutput=%.3f (expected=%.3f), result= %s%n",
-                    i + 1, hour, distance, intraNet, actualOput, expected,result);
+                    i + 1, hour, distance, intraNet, actualOput, expected, result);
         }
     }
 }
